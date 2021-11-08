c25c563d8b8006f70f0ca849b476ebbb9801340f
hnyazkan <93919216+hnyazkan@users.noreply.github.com>
2021-11-08 14:58:10 +0300

    initial commit

---
+++ Console.programlama/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+           Console.WriteLine("Hello World!");
+           Console.WriteLine("İsminizi Giriniz");
+           string name = Console.ReadLine();
+           Console.WriteLine("Soyadınızı Giriniz");
+           string surname = Console.ReadLine();
+
+           Console.WriteLine("Hello " + name + " " + surname );
+           
+        }
+    }
+}
